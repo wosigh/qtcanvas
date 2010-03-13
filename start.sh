8f710d157e093d36c42c78e0a5b5056e83208cdb
Darron Black <darron@griffin.griffin.net>
2010-03-13 00:57:15 -0600

    game type, fix path

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /media/cryptofs/apps/usr/palm/applications/net.griffin.qtdemo
+cd /media/cryptofs/apps/usr/palm/applications/net.griffin.qtcanvas
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/media/cryptofs/apps/usr/palm/applications/net.griffin.qt/qt/lib
 export QWS_DISPLAY=palmpresdlfb
